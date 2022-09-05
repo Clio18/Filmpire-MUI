a25fafa5eb1359b03e191667c9738e2d15942edc
U0L9511 <estclio09@gmail.com>
2022-09-05 17:20:08 +0300

    Add genreIcons: array of pngs

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -13,6 +13,7 @@ import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
 import useStyles from './styles';
 import { useGetGenresQuery } from '../../services/TMDB';
+import genreIcons from '../../assets/genres';
 
 const categories = [
   {
@@ -26,25 +27,6 @@ const categories = [
   {
     label: 'Upcoming',
     value: 'upcoming',
-  },
-];
-
-const demoCategories = [
-  {
-    label: 'Comedy',
-    value: 'comedy',
-  },
-  {
-    label: 'Action',
-    value: 'action',
-  },
-  {
-    label: 'Horror',
-    value: 'horror',
-  },
-  {
-    label: 'Animation',
-    value: 'animation',
   },
 ];
 
@@ -73,9 +55,13 @@ const Sidebar = ({ setMobileOpen }) => {
         {categories.map(({ label, value }) => (
           <Link key={value} className={classes.links} to="/">
             <ListItem onClick={() => {}} button>
-              {/* <ListItemIcon>
-                <img src={redLogo} className={classes.genreImage} height={30} />
-              </ListItemIcon> */}
+              <ListItemIcon>
+                <img
+                  src={genreIcons[label.toLowerCase()]}
+                  className={classes.genreImage}
+                  height={30}
+                />
+              </ListItemIcon>
               <ListItemText primary={label} />
             </ListItem>
           </Link>
@@ -92,9 +78,13 @@ const Sidebar = ({ setMobileOpen }) => {
           data.genres.map(({ name, id }) => (
             <Link key={name} className={classes.links} to="/">
               <ListItem onClick={() => {}} button>
-                {/* <ListItemIcon>
-                <img src={redLogo} className={classes.genreImage} height={30} />
-              </ListItemIcon> */}
+                <ListItemIcon>
+                  <img
+                    src={genreIcons[name.toLowerCase()]}
+                    className={classes.genreImage}
+                    height={30}
+                  />
+                </ListItemIcon>
                 <ListItemText primary={name} />
               </ListItem>
             </Link>
