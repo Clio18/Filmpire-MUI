f9a5848c0d1d07a86611368e9d22873d41ca2fe8
U0L9511 <estclio09@gmail.com>
2022-09-05 11:58:50 +0300

    Redux was added: createApi, fetchBaseQuery, useGetMoviesQuery automatically-generetad hook

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const theme = createTheme({});
 
 root.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
 );
