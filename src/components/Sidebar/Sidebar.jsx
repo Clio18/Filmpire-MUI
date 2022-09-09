780bd3d8a7833ec42179eb98c5b90c3bdd307fef
U0L9511 <estclio09@gmail.com>
2022-09-09 18:30:26 +0300

    Small fixes: avatar and navbar hiding

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -46,6 +46,10 @@ const Sidebar = ({ setMobileOpen }) => {
   const { data, isFetching } = useGetGenresQuery({ genreIdOrCategoryName });
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    setMobileOpen(false);
+  }, [genreIdOrCategoryName]);
+
   return (
     <>
       <Link to="/" className={classes.imageLink}>
